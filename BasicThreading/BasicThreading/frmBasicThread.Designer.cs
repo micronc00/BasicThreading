0db6179be268f4ebe5481f35362f7d253d4df21b
Ron Car Dilao <97319626+micronc00@users.noreply.github.com>
2022-10-27 15:04:45 +0800

    Add files via upload

---
+++ BasicThreading/BasicThreading/frmBasicThread.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace MyThreads
+{
+    partial class frmBasicThread
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Thread = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_Thread
+            // 
+            this.lbl_Thread.AutoSize = true;
+            this.lbl_Thread.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Thread.Location = new System.Drawing.Point(83, 69);
+            this.lbl_Thread.Name = "lbl_Thread";
+            this.lbl_Thread.Size = new System.Drawing.Size(198, 20);
+            this.lbl_Thread.TabIndex = 0;
+            this.lbl_Thread.Text = "-Before starting thread-";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRun.Location = new System.Drawing.Point(136, 127);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(83, 29);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // frmBasicThread
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 203);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.lbl_Thread);
+            this.Name = "frmBasicThread";
+            this.Text = "frmBasicThread";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Thread;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ BasicThreading/BasicThreading/frmBasicThread.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyThreads
+{
+    public partial class frmBasicThread : Form
+    {
+        public frmBasicThread()
+        {
+            InitializeComponent();
+        }
+
+        public void MyThreads()
+        {
+            lbl_Thread.Text = "-Before starting thread-";
+
+
+            MyThreadClass ThreadClass = new MyThreadClass();
+
+            Thread threadA = new Thread(new ThreadStart(ThreadClass.thread1));
+
+            Thread threadB = new Thread(new ThreadStart(ThreadClass.thread2));
+
+            threadA.Name = "Thread A Process";
+            Console.WriteLine("-Before starting thread-");
+            threadA.Start();
+
+            threadB.Name = "Thread B Process";
+            Console.WriteLine("-Before starting thread-");
+            threadB.Start();
+
+            threadA.Join();
+            threadB.Join();
+
+            Console.WriteLine("-Ends of Thread-");
+
+            lbl_Thread.Text = "-Ends of Thread-";
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            MyThreads();
+        }
+    }
+}
